5c908c3ed542a38cb2609aeaa0a37b2f06b3e5c5
Destiny Saturday <yiradesat@gmail.com>
2023-05-08 22:02:04 +0100

    A new Journey - Hello, World!

---
+++ hello.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * Author: Destiny Saturday
+ * Date: Mon 8/05/2023
+ * Purpose: Prints "Hello, world!" followed by a new line
+ */
+
+int main(void)
+{
+	printf("Hello, world!\n");
+	return (0);
+}

741fed928a8d36be4c815e02889360e60bb8118c
Destiny Saturday <yiradesat@gmail.com>
2023-05-08 22:56:24 +0100

    Formatted description comment, added poem.c

--- hello.c
+++ hello.c
@@ -6,8 +6,9 @@
  * Return: Always 0 (Success)
  *
  * Author: Destiny Saturday
- * Date: Mon 8/05/2023
- * Purpose: Prints "Hello, world!" followed by a new line
+ * Date: 08-05-2023
+ *
+ * Description: Prints "Hello, world!" followed by a new line
  */
 
 int main(void)

39ebd5b716b4d5cd7415b7e1a0a72a794e64d843
Destiny Saturday <yiradesat@gmail.com>
2023-05-09 16:36:35 +0100

    ADded poem.c (Roses, Violet, Sugar and you.)

--- poem.c
+++ poem.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * 
- * Ret
+ * main - Entry point, Prtints a poem
+ *
+ * Return: Always 0 (Success)
+ *
+ * Author: Destiny Saturday
+ * Date: 09-05-2023
 */
+
+int main(void)
+{
+	printf("Roses are red\n");
+	printf("Violets are blue\n");
+	printf("Sugar is sweet\n");
+	printf("And so are you\n");
+	return (0);
+}
